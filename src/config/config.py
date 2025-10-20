5192fe70a10ea686e41eff1115afa338b2f18677
amitkravchik <amitkravchik@gmail.com>
2025-10-20 21:16:39 +0300

    config dir

---
+++ src/config/config.py
@@ -0,0 +1,2 @@
+DATASET_URL = "https://raw.githubusercontent.com/ybisk/ybisk.github.io/master/piqa/data/train.jsonl"
+LABELS_URL = "https://raw.githubusercontent.com/ybisk/ybisk.github.io/master/piqa/data/train-labels.lst"
